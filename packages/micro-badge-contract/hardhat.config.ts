d72cc8234d49c24349eadae23dceaeeb9f5b63e2
pasha <robbyprawira3@gmail.com>
2025-05-15 23:53:22 +0700

    Hardhat setup

---
+++ packages/micro-badge-contract/hardhat.config.ts
@@ -0,0 +1,21 @@
+import type { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox-viem";
+
+const config: HardhatUserConfig = {
+	defaultNetwork: "hardhat",
+	solidity: "0.8.28",
+	paths: {
+		sources: "./contracts",
+		tests: "./test",
+		cache: "./cache/",
+		artifacts: "./artifacts/",
+	},
+	networks: {
+		localhost: {
+			url: "http://127.0.0.1:8545",
+		},
+		hardhat: {},
+	},
+};
+
+export default config;
