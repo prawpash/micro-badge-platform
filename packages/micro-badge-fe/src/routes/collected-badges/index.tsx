9546bd7130aa271bbd8495f0afcf10efaa2bdc3d
prawpash <59055214+prawpash@users.noreply.github.com>
2025-05-17 18:37:46 +0700

    Feat: All Page (#3)
    * feat: Home Page
    
    Create a home page without actions to do anything
    
    * feat: Separate into several components
    
    Separating the code for header, footer, and layout
    
    * feat: Issue a Badge Page
    
    Create new page for issuing a badge
    
    * feat: Claim a Badge Page
    
    Create a page for claiming the badge
    
    * feat: Update Metadata
    
    Updating the metadata on issue a badge page
    
    * feat: Collected Badges
    
    Create a page to displaying the collected badges that user have
    
    * feat: Detail Badge
    
    Updating the style and layout that used to show detail badge
    
    * feat: Issued Page
    
    Create a page for listing the created badges
    
    * feat: Update Metadata
    
    Updating metadata for Collected Badges page

---
+++ packages/micro-badge-fe/src/routes/collected-badges/index.tsx
@@ -0,0 +1,104 @@
+import { component$ } from '@builder.io/qwik';
+import { Link, type DocumentHead } from '@builder.io/qwik-city';
+import { LuChevronLeft, LuSearch, LuExternalLink } from '@qwikest/icons/lucide';
+
+export default component$(() => {
+	return (
+		<>
+			{/* body */}
+			<div class="flex-1 flex flex-col">
+				{/* navigation section */}
+				<div class="flex px-4 mt-5">
+					<Link
+						href="/"
+						class="btn btn-primary btn-circle p-2 w-auto h-auto border-0"
+					>
+						<div style={{ color: 'var(--color-base-100)', fontSize: '40px' }}>
+							<LuChevronLeft />
+						</div>
+					</Link>
+				</div>
+				{/* end navigation section */}
+
+				{/* list of collected badges */}
+				<div class="mt-7 mx-auto flex flex-row gap-x-28">
+					<div class="flex flex-col w-sm bg-base-100 rounded-2xl p-4 relative">
+						{/* open detail button */}
+						<Link class="absolute top-2 right-2 bg-accent p-2 btn btn-circle border-0">
+							<div style={{ fontSize: '20px', color: 'var(--color-base-100)' }}>
+								<LuExternalLink />
+							</div>
+						</Link>
+						{/* head */}
+						<div class="flex flex-col w-full mt-3">
+							<div class="w-[80%] self-center">
+								<div class="responsive aspect-video bg-accent rounded-2xl"></div>
+							</div>
+							<h1 class="text-2xl text-center mt-4">Badge Name</h1>
+						</div>
+						{/* body */}
+						<div class="flex flex-col gap-y-2 mt-4">
+							<div class="flex flex-row gap-x-2">
+								<h2>Issuer :</h2>
+								<p>0x0921423152314cavsd4321</p>
+							</div>
+							<div class="flex flex-row gap-x-2">
+								<h2>Owner :</h2>
+								<p>0x0921423152314cavsd4321</p>
+							</div>
+							<div class="flex flex-row gap-x-2">
+								<h2>Expiration Date :</h2>
+								<p>02 May 2026</p>
+							</div>
+							<div class="flex flex-col gap-y-1">
+								<h2>Description :</h2>
+								<p>
+									Lorem Ipsum is simply dummy text of the printing and
+									typesetting industry. Lorem Ipsum has been the industry's
+									standard dummy text ever since the 1500s, when an unknown
+									printer took a galley of type and scrambled it to make a type
+								</p>
+							</div>
+						</div>
+					</div>
+
+					<div class="flex flex-col">
+						<div class="ml-auto">
+							<label class="input w-sm">
+								<div
+									style={{ fontSize: '16px', color: 'var(--color-base-100)' }}
+									class="bg-primary rounded-full p-1"
+								>
+									<LuSearch />
+								</div>
+								<input type="search" required placeholder="Search" />
+							</label>
+						</div>
+
+						<div class="w-2xl bg-base-100 rounded-2xl max-h-130 relative mt-2">
+							<div class="grid grid-cols-5 gap-6 max-w-full w-screen max-h-full p-8 pr-2 overflow-auto">
+								{Array.from({ length: 20 }, (_, i) => `dummy ${i}`).map((i) => (
+									<div key={i} class="w-25 h-25 bg-accent rounded-2xl"></div>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+				{/* end list of collected badges */}
+			</div>
+
+			{/* end body */}
+		</>
+	);
+});
+
+export const head: DocumentHead = {
+	title: 'Claim a Badge',
+	meta: [
+		{
+			name: 'Page for claiming a badge',
+			content:
+				'Application for issuing and claiming badges in decentralized way',
+		},
+	],
+};
--- packages/micro-badge-fe/src/routes/index.tsx
+++ packages/micro-badge-fe/src/routes/index.tsx
@@ -1,27 +1,140 @@
-import { component$, Slot } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
-import { QWeb3Provider } from '~/integrations/react/web3-provider';
+import { component$ } from '@builder.io/qwik';
+import { Link, type DocumentHead } from '@builder.io/qwik-city';
+import { LuBadgeCheck } from '@qwikest/icons/lucide';
+// import { QWeb3Provider } from '~/integrations/react/web3-provider';
 
+// export default component$(() => {
+// 	return (
+// 		<div class="w-screen h-screen flex flex-col items-center justify-center">
+// 			<div class="join">
+// 				<button class="btn join-item w-50 h-50">Issue Badge</button>
+// 				<button class="btn join-item w-50 h-50">Claim Badge</button>
+// 			</div>
+// 			<div>
+// 				<QWeb3Provider></QWeb3Provider>
+// 			</div>
+// 		</div>
+// 	);
+// });
 export default component$(() => {
 	return (
-		<div class="w-screen h-screen flex flex-col items-center justify-center">
-			<div class="join">
-				<button class="btn join-item w-50 h-50">Issue Badge</button>
-				<button class="btn join-item w-50 h-50">Claim Badge</button>
+		<>
+			{/* body */}
+			<div class="flex-1 flex flex-col items-center justify-center">
+				<div class="flex flex-col gap-y-12">
+					{/* issue and claim badge */}
+					<div class="flex flex-row gap-x-25">
+						{/* issue a badge	 */}
+						<Link
+							href="/issue-badge"
+							class="btn btn-ghost h-auto w-auto p-0 m-0 hover:bg-transparent hover:border-transparent"
+						>
+							<div
+								class="card bg-base-100 w-2xs shadow-sm aspect-square"
+								style={{ boxShadow: '10px 20px 0px 0px #181F2A;' }}
+							>
+								<div class="card-body flex flex-col items-center justify-center">
+									<div class="flex flex-col items-center gap-y-2">
+										<div
+											style={{
+												fontSize: '70px',
+												color: 'var(--color-primary)',
+											}}
+										>
+											<LuBadgeCheck
+												style={{
+													fill: 'var(--color-primary)',
+													stroke: 'var(--color-base-100)',
+													strokeWidth: '1px',
+												}}
+											/>
+										</div>
+										<h1 class="text-4xl font-semibold">Issue a Badge</h1>
+									</div>
+								</div>
+							</div>
+						</Link>
+
+						{/* create a badge	 */}
+						<Link
+							href="/claim-badge"
+							class="btn btn-ghost h-auto w-auto p-0 m-0 hover:bg-transparent hover:border-transparent"
+						>
+							<div
+								class="card bg-base-100 w-2xs shadow-sm aspect-square"
+								style={{ boxShadow: '10px 20px 0px 0px #181F2A;' }}
+							>
+								<div class="card-body flex flex-col items-center justify-center">
+									<div class="flex flex-col items-center gap-y-2">
+										<div
+											style={{
+												fontSize: '70px',
+												color: 'var(--color-primary)',
+											}}
+										>
+											<LuBadgeCheck
+												style={{
+													fill: 'var(--color-primary)',
+													stroke: 'var(--color-base-100)',
+													strokeWidth: '1px',
+												}}
+											/>
+										</div>
+										<h1 class="text-4xl font-semibold">Claim a Badge</h1>
+									</div>
+								</div>
+							</div>
+						</Link>
+					</div>
+					{/* end issue and claim badge */}
+
+					{/* my badges	 */}
+					<div>
+						<Link
+							href="/collected-badges"
+							class="btn btn-ghost h-auto w-full p-0 m-0 hover:bg-transparent hover:border-transparent"
+						>
+							<div
+								class="card bg-base-100 w-full shadow-sm aspect-3/1"
+								style={{ boxShadow: '10px 20px 0px 0px #181F2A;' }}
+							>
+								<div class="card-body flex flex-col items-center justify-center">
+									<div class="flex flex-col items-center gap-y-2">
+										<div
+											style={{
+												fontSize: '70px',
+												color: 'var(--color-primary)',
+											}}
+										>
+											<LuBadgeCheck
+												style={{
+													fill: 'var(--color-primary)',
+													stroke: 'var(--color-base-100)',
+													strokeWidth: '1px',
+												}}
+											/>
+										</div>
+										<h1 class="text-4xl font-semibold">Collected Badges</h1>
+									</div>
+								</div>
+							</div>
+						</Link>
+					</div>
+					{/* end my badges	 */}
+				</div>
 			</div>
-			<div>
-				<QWeb3Provider></QWeb3Provider>
-			</div>
-		</div>
+			{/* end body */}
+		</>
 	);
 });
 
 export const head: DocumentHead = {
-	title: 'Welcome to Qwik',
+	title: 'Home',
 	meta: [
 		{
-			name: 'description',
-			content: 'Qwik site description',
+			name: 'Home page for micro badge application',
+			content:
+				'Application for issuing and claiming badges in decentralized way',
 		},
 	],
 };
