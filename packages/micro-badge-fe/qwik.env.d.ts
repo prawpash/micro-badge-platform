3d1cf2ae5e7417d7880d20d15bf6550d6bc8a03c
pasha <robbyprawira3@gmail.com>
2025-05-17 18:35:51 +0700

    Revert "Feat: All Page (#2)"
    This reverts commit 26eb4de2cd681576ef7c05ee1dadc6aa8db99dad.

--- packages/micro-badge-fe/qwik.env.d.ts
+++ packages/micro-badge-fe/qwik.env.d.ts
@@ -2,13 +2,3 @@
 
 // Add global `vite/client` types. For more info, see: https://vitejs.dev/guide/features#client-types
 /// <reference types="vite/client" />
-
-interface ImportMetaEnv {
-	readonly PUBLIC_XELLAR_ENV: 'sandbox' | 'production';
-	readonly PUBLIC_WALLET_CONNECT_PROJECT_ID: string;
-	readonly PUBLIC_XELLAR_APP_ID: string;
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv;
-}
--- packages/micro-badge-fe/src/routes/layout.tsx
+++ packages/micro-badge-fe/src/routes/layout.tsx
@@ -1,28 +1,17 @@
-import { component$, Slot } from '@builder.io/qwik';
-import type { RequestHandler } from '@builder.io/qwik-city';
-import Footer from '~/components/footer';
-import Header from '~/components/header';
+import { component$, Slot } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler = async ({ cacheControl }) => {
-	// Control caching for this request for best performance and to reduce hosting costs:
-	// https://qwik.dev/docs/caching/
-	cacheControl({
-		// Always serve a cached response by default, up to a week stale
-		staleWhileRevalidate: 60 * 60 * 24 * 7,
-		// Max once every 5 seconds, revalidate on the server to get a fresh version of this page
-		maxAge: 5,
-	});
+  // Control caching for this request for best performance and to reduce hosting costs:
+  // https://qwik.dev/docs/caching/
+  cacheControl({
+    // Always serve a cached response by default, up to a week stale
+    staleWhileRevalidate: 60 * 60 * 24 * 7,
+    // Max once every 5 seconds, revalidate on the server to get a fresh version of this page
+    maxAge: 5,
+  });
 };
 
 export default component$(() => {
-	return (
-		<div
-			class="default w-screen h-screen flex flex-col bg-base-200"
-			data-theme="defaultProject"
-		>
-			<Header />
-			<Slot />
-			<Footer />
-		</div>
-	);
+  return <Slot />;
 });
