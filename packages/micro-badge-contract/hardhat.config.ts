4216bff7da4c4a9722d2d2cc3d3c3fbb645407e0
pasha <robbyprawira3@gmail.com>
2025-05-18 21:41:12 +0700

    feat: Deploy config
    Deploy configuration

--- packages/micro-badge-contract/hardhat.config.ts
+++ packages/micro-badge-contract/hardhat.config.ts
@@ -1,5 +1,6 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
 	defaultNetwork: "hardhat",
@@ -15,6 +16,30 @@ const config: HardhatUserConfig = {
 			url: "http://127.0.0.1:8545",
 		},
 		hardhat: {},
+		liskSepolia: {
+			url: "https://rpc.sepolia-api.lisk.com",
+			accounts: [process.env.REAL_PERSONAL_WALLET_KEY as string],
+			gasPrice: 1000000000,
+		},
+	},
+	etherscan: {
+		// Use "123" as a placeholder, because Blockscout doesn't need a real API key, and Hardhat will complain if this property isn't set.
+		apiKey: {
+			"lisk-sepolia": "123",
+		},
+		customChains: [
+			{
+				network: "lisk-sepolia",
+				chainId: 4202,
+				urls: {
+					apiURL: "https://sepolia-blockscout.lisk.com/api",
+					browserURL: "https://sepolia-blockscout.lisk.com",
+				},
+			},
+		],
+	},
+	sourcify: {
+		enabled: false,
 	},
 };
 
